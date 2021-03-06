78706be0fc9ddef0b425d56a6e691ced950b11e8
cmosdiamond <jupiter1302@gmail.com>
2021-03-06 18:37:45 +0700

    Add files via upload

--- test/pageObjects/Login.page.js
+++ test/pageObjects/Login.page.js
@@ -1,18 +1,13 @@
 class Login {
     get $email () { return $('input[id="loginEmail"]'); }
     get $password () { return $('input[placeholder="Enter Password"]'); }
-    get $submit () { return $('button[type="submit"]'); }
+    get $submit () { return $('button:contains(^Login$)'); }
 
     login ({ email, password }) {
         this.$email.setValue(email);
         this.$password.setValue(password);
 
-	this.$submit.addEventListener('keyup', function(event) {
-    if (event.code === 'Enter') {
-      event.preventDefault();
-      document.querySelector('form').submit();
-    }});
-	
+	this.$submit.click();	
 	
     }
 }
